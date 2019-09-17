ce3fd374854779398c57dfd4cbad5a304b3f5613
Joe Ladson <jladson@amplify.com>
2019-09-17 15:11:44 -0400

    adding initial commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,141 @@
+/*
+Variables and data types
+*/
+
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+//console.log(lastName);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// variables naming rules
+var _3years = 3;
+var johnmark = 'john and mark';
+var if = 23;
+*/
+
+
+/*
+Variables and mutations and type coertion 
+*/
+
+/*
+var firstName = 'John';
+var age = 28;
+
+// Type Coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' +  age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' +  age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+
+/**********
+ *  Basic Operators
+ */
+
+ /*
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+var yearJohn = now - ageJohn;
+var yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+
+// typeof operators
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+*/
+
+/*******
+ *  Operators precedence 
+ */
+
+
+ /*
+ var now = 2018;
+ var yearJohn = 1989;
+ var fullAge = 18;
+
+ // Multiple Operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge);
+
+//Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// More operators
+// = x * 2;
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+
+x = x + 1;
+x += 1;
+x++;
+*/
+
+
+
+/*********
+ *  Coding Challenge 1
+ */
+
+/*
+Mark and John are trying to compare their
+
+*/
+
+
+
+
+
