faca8e07a22bab276cb931daffb81d303d2f32de
Joe Ladson <jladson@amplify.com>
2019-10-15 14:23:21 -0400

    updatingscript file

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -143,7 +143,7 @@ BMI = mass / height^2 = mass / (height * height).
 Good Luck
 */
 
-/*
+var distance = 1;
 var massMark = 78; // kg
 var heightMark =  1.69; // meters
 
@@ -156,7 +156,6 @@ console.log(BMIMark, BMIJohn);
 
 var markHigherBMI = BMIMark > BMIJohn;
 console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
-*/
 
 
 /*************************
@@ -303,3 +302,71 @@ if (age < 13) {
 /***********************
  * Truthy and falsy values and equality operators
  */
+
+// falsy values: undefined, null, 0, '', 
+// truthy values: Not falsy values
+
+var height; 
+height = 0;
+
+if (height) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variables has NOT been defined');
+}
+
+/*****************************
+* CODING CHALLENGE 2
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+GOOD LUCK 😀
+*/
+
+
+/******************************
+ * Functions
+ */
+
+ function calculateAge(birthYear) {
+     return 2019 - birthYear
+ }
+
+ var ageJohn = calculateAge(1990);
+ var ageMike = calculateAge(1948);
+ var ageJane = calculateAge(1969);
+
+ console.log(ageJohn, ageMike, ageJane);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
