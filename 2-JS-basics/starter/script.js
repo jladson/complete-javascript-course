ab281a304d605abfbd819e955c087c1117e141e5
Joe Ladson <jladson@amplify.com>
2019-10-16 11:29:14 -0400

    updating starter-script file

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -334,15 +334,87 @@ GOOD LUCK 😀
  * Functions
  */
 
+ /*
  function calculateAge(birthYear) {
-     return 2019 - birthYear
+     return 2018 - birthYear
  }
 
  var ageJohn = calculateAge(1990);
  var ageMike = calculateAge(1948);
  var ageJane = calculateAge(1969);
-
  console.log(ageJohn, ageMike, ageJane);
+
+ function yearsUntilRetirement(year, firstName)
+ {
+     var age = calculateAge(year);
+     var retirement = 65 - age;
+
+     if (retirement > 0 ) {
+     console.log(firstName + ' retires in ' + retirement + ' years.');
+      } else {
+          console.log(firstName + ' is already retired.')
+      } 
+     
+ }
+
+ yearsUntilRetirement(1990, 'John');
+ yearsUntilRetirement(1948, 'Mike');
+ yearsUntilRetirement(1969, 'Jane');
+/*
+
+ /*********************
+  * Function Statement and Expressions 
+  */
+
+  /*
+  // Function declaration
+  var whatDoYouDo = function(job, firstName){}
+
+  // Function expression
+  var whatDoYouDo = function(job, firstName){
+      switch(job) {
+          case 'teacher':
+              return firstName + ' teaches kids how to code';
+
+          case "driver":
+              return firstName + ' drives a cab in lisbon';
+
+          case "designer":
+              return firstName + ' designs beautiful websites';
+          default:
+              return firstName + ' does something else';
+      }
+  }
+
+  console.log(whatDoYouDo('teacher', 'John'));
+  console.log(whatDoYouDo('designer', 'Jane'));
+  console.log(whatDoYouDo('retired', 'Mark'));
+  console.log(whatDoYouDo('driver', 'Bob'));
+  */
+
+
+  /********************
+   *  Arrays
+   */
+
+   var names = ['John', 'Jane', 'Mark'];
+   var years = new Array(1990, 1969, 1948);
+
+   console.log(names[0]);
+   console.log(names);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
