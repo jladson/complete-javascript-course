745b20fb3669417197ae2ef42aa596a83511feb6
Joe Ladson <jladson@amplify.com>
2019-10-17 12:39:06 -0400

    updating script JS file.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -397,11 +397,47 @@ GOOD LUCK 😀
    *  Arrays
    */
 
+   // Initiate array data
    var names = ['John', 'Jane', 'Mark'];
    var years = new Array(1990, 1969, 1948);
 
    console.log(names[0]);
    console.log(names);
+   console.log(names.length);
+
+   // Mutate array data
+   names[1] = 'Ben';
+   names[names.length] = 'Mary';
+   console.log(names);
+
+   // Different data types
+   var john = ['John', 'Smith', 1990, 'teacher', 'designer', false];
+
+   john.push('blue');
+   john.unshift('Mr.');
+   console.log(john);
+
+   john.pop();
+   john.pop();
+   john.shift();
+   console.log(john);
+
+   console.log(john.indexOf(1990));
+
+   var isDesigner = john.indexOf('designer') === -1 ? 'John is Not a designer' : 'John IS a designer';
+   console.log(isDesigner);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
